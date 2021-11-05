06e056e1c0a87e717aeab5979d0258e8eca0544c
Mohamad Bashar Karim <46751800+Karim145@users.noreply.github.com>
2021-11-05 19:28:22 +0100

    Update Iteration7.py

--- vecka1/Iteration7/Iteration7.py
+++ vecka1/Iteration7/Iteration7.py
@@ -1,24 +1,3 @@
-import requests
-import random
-
-
-QUIZ_URL = "https://bjornkjellgren.se/quiz/v2/questions"
-response = requests.get(QUIZ_URL)
-
-try:
-    response.raise_for_status()
-except requests.HTTPError as e:
-    print("Connection error"),exit()
-response =response.json()
-
-randomlist=random.sample(list(filter(lambda x : int(x['id']), response['questions'])), k=10)
-
-
-def post_svaret(question_nr, ditt_svar):
-    r=requests.get(QUIZ_URL).json()
-    payload = {"id":question_nr ,"Rätt":ditt_svar}
-    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-    r= requests.post(QUIZ_URL, json=payload, headers=headers)
 
 class Answer:
     answer: str
@@ -38,7 +17,7 @@ def parse_answers(answers) :
     return res
 
 
-class Question(Answer):
+class Question(Answer): #Den här klassen bearbetar frågorna och beräknar procentandelen av korrekta svar
 
     id: int
     prompt: str
