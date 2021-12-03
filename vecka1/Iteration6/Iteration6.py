98fc27c0020b1974237a2046a0f05d52dd059e93
Karim145 <bashar.krayem14@gmail.com>
2021-12-03 16:01:12 +0100

    Mainfunktionen är rätt strukterad nu

--- vecka1/Iteration6/Iteration6.py
+++ vecka1/Iteration6/Iteration6.py
@@ -67,9 +67,9 @@ class Statistik(Question,Answer):
 def check_correct_input(your_answer,antal_svar):
     if your_answer>antal_svar or your_answer<=0:
         print("-" * 80)
-        print(f" **** Du kommer att få svara på frågorna på nytt!! ****")
-        print(f"Frågan har {antal_svar} svar, och endast siffror 1-{antal_svar} accepteras som input. Försök igen med bokstaver "),main()
-
+        #print(f" **** Du kommer att få svara på frågorna på nytt!! ****")
+        print(f"Frågan har {antal_svar} svar, och endast siffror 1-{antal_svar} accepteras som input. Försök igen! ")
+        return "error"
 def get_questions():
     res = []
     for q in randomlist:
@@ -79,53 +79,61 @@ def get_questions():
 
 def main():
 
-    try:
 
-        antal=len(randomlist)
-        print("-" * 80),print("Du kommer att få svara på 10 slumpmässiga frågor från API:ET: "),print("-" * 80)
-        correct_svar=0
-        list_of_svarade_fel_question=[]
-        control_svar=0
-        for question in get_questions():
-            times_correct=question[3]
-            times_asked=question[2]
-            print(f'Fråga {question[0]}. [{percent_correct((times_correct,times_asked))} har svarat rätt]. {question[1]}')
-            global i, msg
+    antal=len(randomlist)
+    print("-" * 80),print("Du kommer att få svara på 10 slumpmässiga frågor från API:ET: "),print("-" * 80)
+    correct_svar=0
+    list_of_svarade_fel_question=[]
+    control_svar=0
+
+    for question in get_questions():
+        times_correct=question[3]
+        times_asked=question[2]
+        global i, msg
+
+        while True:
+            print(f'Fråga {question[0]}. [{percent_correct((times_correct, times_asked))} har svarat rätt]. {question[1]}')
             for i, answer in enumerate((question[4]), start=1):
-                answer1=answer[1]
-                answer=answer[0]
+                answer1 = answer[1]
+                answer = answer[0]
                 print(f"[{i}] {answer}")
                 if answer1:
-                    control_svar=int(i)
-                    msg= f"rätt svar är {i}. {answer}"
-            antal_svar=i
-            your_answer = int(input(f"Svara här --> "))
-            check_correct_input(int(your_answer),int(antal_svar))
-            print(f'Ditt svar. {your_answer}')
-            if int(control_svar)==int(your_answer):
-                correct_answer = f"Rätt!!"
-                correct_svar+=1
-                ditt_svar=True
-            else:
-                correct_answer = f'Fel,{msg}'
-                list_of_svarade_fel_question.append(f'{question[0]}. {question[1]}')
-                list_of_svarade_fel_question.append(f' -> {msg}')
-                ditt_svar=False
-            print(correct_answer)
+                    control_svar = int(i)
+                    msg = f"rätt svar är {i}. {answer}"
+            antal_svar = i
+            try:
+                your_answer = input(f"Svara här --> ")
+                check_error= check_correct_input(int(your_answer),int(antal_svar))
+                if check_error == "error":
+                    raise ValueError
+                else:
+                    print(f'Ditt svar. {your_answer}')
+                    if int(control_svar)==int(your_answer):
+                        correct_answer = f"Rätt!!"
+                        correct_svar+=1
+                        ditt_svar=True
+                    else:
+                        correct_answer = f'Fel,{msg}'
+                        list_of_svarade_fel_question.append(f'{question[0]}. {question[1]}')
+                        list_of_svarade_fel_question.append(f' -> {msg}')
+                        ditt_svar=False
+                    print(correct_answer)
 
-            post_svaret(question[0], ditt_svar)
-            print("-" * 80)
+                    post_svaret(question[0], ditt_svar)
+                    print("-" * 80)
+                    break
+            except ValueError:
+                print("Det var ingen siffra!  Var vänlig och försök igen! ")
+                print("-" * 80)
 
 
-        print(f'*** RESULTAT ***')
-        print(f'Du fick {correct_svar} av {antal} möjliga poäng.')
-        if len(list_of_svarade_fel_question) > 0:
-            print(f'Du svarade fel på dessa frågor:')
-            print(*list(list_of_svarade_fel_question), sep = '\n' )
+    print(f'*** RESULTAT ***')
+    print(f'Du fick {correct_svar} av {antal} möjliga poäng.')
+    if len(list_of_svarade_fel_question) > 0:
+        print(f'Du svarade fel på dessa frågor:')
+        print(*list(list_of_svarade_fel_question), sep = '\n' )
 
-    except ValueError:
-        print("-" * 80)
-        print("Det var ingen siffra!  Var vänlig och försök igen! "),main()
+    #,main()
 
 if __name__ == '__main__':
     main()
